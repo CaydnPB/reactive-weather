b1c7aba0d2aeb5e2df7204b39a6e6f392eb19378
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-14 17:10:07 +0000

    Add conditional icons

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg"
-
+import rainy from "../assets/Rainy.svg"
+import partlyCloudy from "../assets/partlyCloudy.svg"
+import cloudy from "../assets/Cloudy.svg"
 
 function WeatherCard(props) {
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src={(() => {
+  switch (props.data.forecast) {
+    case 'Sunny':
+      return sunny;
+    case 'Rainy':
+      return rainy;
+    case 'Partly cloudy':
+      return partlyCloudy;
+    case 'Cloudy':
+      return cloudy;
+    default:
+      return partlyCloudy;
+  }})()} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
             <h3 className="card-title">{props.data.city}</h3>
